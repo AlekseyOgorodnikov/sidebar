451fd29155d71f789369b9723d0ef2c473617a34
AlekseyOgorodnikov <ogorodnikovalexey@gmail.com>
2020-09-29 20:37:17 +0400

    finally

---
+++ src/pages/Support.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Support = () => {
+    return (
+        <div className='support'>
+            <h1>Supports</h1>
+        </div>
+    );
+}
+
+export default Support
